9eff5e14e3ad7d20dab3f6b0fec79368a4779534
Kiran Joy <kijoyin@gmail.com>
2019-11-25 21:59:36 +1000

    DynamoDbHealthCheckExtensions

---
+++ DynamoDBHealthCheck/DynamoDbHealthCheckExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamoDBHealthCheck
+{
+    public static class DynamoDbHealthCheckExtensions
+    {
+        const string NAME = "dynamodb";
+        public static IHealthChecksBuilder AddDynamoDb(this IHealthChecksBuilder builder, Action<DynamoOptions> setup, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default, TimeSpan? timeout = default)
+        {
+            var options = new DynamoOptions();
+            setup?.Invoke(options);
+
+            return builder.Add(new HealthCheckRegistration(
+                name ?? NAME,
+                sp => new DynamoHealth(options),
+                failureStatus,
+                tags,
+                timeout));
+        }
+    }
+}
